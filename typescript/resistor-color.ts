ba2e50f49ec1af1b29a1f93fa5bca053d99d28c8
colour-of-art <149279801+colour-of-art@users.noreply.github.com>
2023-12-17 16:41:30 +1100

    Create resistor-color.ts

---
+++ typescript/resistor-color.ts
@@ -0,0 +1,4 @@
+export const colorCode = (color : string) => {
+  return COLORS.indexOf(color);
+}
+export const COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"];
